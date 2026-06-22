e9a65b268b4f78837431db756bb73ce51ced2133
franalejandro <proyectoapoloiv@gmail.com>
2026-06-22 00:49:13 -0400

    feat: fase 1 - esqueleto ingesta-service y docker compose base

---
+++ ingesta-service/src/main/java/com/angloNorte/ingesta/IngestaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.angloNorte.ingesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestaServiceApplication.class, args);
+	}
+
+}
---
+++ ingesta-service/src/test/java/com/angloNorte/ingesta_service/IngestaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angloNorte.ingesta_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
